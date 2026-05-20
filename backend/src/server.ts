ff8a800fa053275ad08985cc94f0d2a0b12ebcbe
shish <you@example.com>
2026-05-20 23:46:48 +0200

    reorganising files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
-import "./lib/error-capture";
+import "../../shared/src/lib/error-capture";
 
-import { consumeLastCapturedError } from "./lib/error-capture";
-import { renderErrorPage } from "./lib/error-page";
+import { consumeLastCapturedError } from "../../shared/src/lib/error-capture";
+import { renderErrorPage } from "../../shared/src/lib/error-page";
 
 type ServerEntry = {
   fetch: (request: Request, env: unknown, ctx: unknown) => Promise<Response> | Response;
